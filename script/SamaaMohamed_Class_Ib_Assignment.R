ec6d44aafe5a43288dee39d5e280e62aaa7a5ea4
sam <samaamohamedabdo5@gmail.com>
2025-08-06 05:30:10 +0300

    Update SamaaMohamed_Class_Ib_Assignment.R

--- script/SamaaMohamed_Class_Ib_Assignment.R
+++ script/SamaaMohamed_Class_Ib_Assignment.R
@@ -1,3 +1,6 @@
+# Samaa Mohamed_Class_Ib
+
+
 getwd()
 
 # creat folders
@@ -8,19 +11,21 @@ dir.create("results")
 # import data
 data = read.csv("data/patient_info.csv")
 
-#
+# Viewing the data structure
 class(data)
 head(data)
 str(data)
 summary(data)
 
-# 
+# CONVERSIONS
 data$gender = as.factor(data$gender)
 data$cancer_status = as.factor(data$diagnosis)
 data$smoker = as.factor(data$smoker)
 data$age = as.numeric(data$age)
 data$bmi = as.numeric(data$bmi)
 
+
+# Save as CSV
 write.csv(data, "data/patient_info_cleaned.csv")
 
 
